3203a76fea5a25b769f6eb72f95ca78bf507de87
official <hisokaff88@gmail.com>
2025-08-04 13:49:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ if __name__ == "__main__":
     keep_alive()
     try:
         asyncio.run(run_bot())
-    except RuntimeError:
-        import nest_asyncio
-        nest_asyncio.apply()
-        asyncio.get_event_loop().run_until_complete(run_bot())
+    except RuntimeError as e:
+        if "Event loop is closed" in str(e):
+            pass  # Ignore this error on shutdown
+        else:
+            import nest_asyncio
+            nest_asyncio.apply()
+            asyncio.get_event_loop().run_until_complete(run_bot())
+    except Exception as e:
+        print(f"Fatal error: {e}")
