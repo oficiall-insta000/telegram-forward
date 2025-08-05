f175856c99554e4b40511380dbf7534feede2f52
official <hisokaff88@gmail.com>
2025-08-05 18:09:05 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-# keep_alive.py
 from aiohttp import web
 import asyncio
 import logging
@@ -12,15 +11,17 @@ class KeepAlive:
         self.site = None
 
     async def health_check(self, request):
-        return web.Response(text="Bot is alive! <h1>SIDO</h1>")
+        return web.Response(text="Bot is alive!")
 
     async def start(self):
         self.runner = web.AppRunner(self.app)
         await self.runner.setup()
         self.site = web.TCPSite(self.runner, '0.0.0.0', self.port)
         await self.site.start()
-        print(f"Keep-alive server running on port {self.port}")
+        logging.info(f"Keep-alive server running on port {self.port}")
 
     async def stop(self):
-        await self.site.stop()
-        await self.runner.cleanup()
+        if self.site:
+            await self.site.stop()
+        if self.runner:
+            await self.runner.cleanup()
